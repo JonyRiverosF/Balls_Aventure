a52c2101029dd7b4b3ccfae6d2ecd62f0297aad0
JonyRiverosF <jo.riverosf@duocuc.cl>
2023-08-26 00:32:17 -0400

    animacion angular

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {MatButtonModule} from '@angular/material/button';
 
 
 
@@ -13,6 +14,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    MatButtonModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
