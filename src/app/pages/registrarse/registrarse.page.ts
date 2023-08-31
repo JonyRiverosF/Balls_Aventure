c284ea2dd7fd589757e60e816ccf8c9e32700713
JonyRiverosF <jo.riverosf@duocuc.cl>
2023-08-30 22:56:18 -0400

    registrarse

--- src/app/pages/registrarse/registrarse.page.ts
+++ src/app/pages/registrarse/registrarse.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-registrarse',
@@ -10,42 +11,47 @@ import { AlertController } from '@ionic/angular';
 export class RegistrarsePage implements OnInit {
 
   formularioRegistro:FormGroup;
+  
 
   constructor(public fb:FormBuilder,public alertController:AlertController) {
     
     this.formularioRegistro=this.fb.group({
       'nombre': new FormControl("",[Validators.required,Validators.minLength(3)]),
-      'contraseña': new FormControl("",[Validators.required,Validators.minLength(5),Validators.maxLength(15),Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/)]),
-      'Confirmar_contraseña': new FormControl("",Validators.required),
+      'contraseña': new FormControl("",[Validators.required,Validators.minLength(5),Validators.maxLength(15),Validators.pattern(new RegExp("(?=.*[0-9])")),Validators.pattern(new RegExp("(?=.*[A-Z])")),Validators.pattern(new RegExp("(?=.*[a-z])")),Validators.pattern(new RegExp("(?=.*[$@^!%*?&])"))]),
+      'Confirmar_contraseña': new FormControl("",[Validators.required]),
       'Correo': new FormControl("",[Validators.required,Validators.minLength(5),Validators.email]),
-      'Respuesta': new FormControl("",[Validators.required,Validators.minLength(5),Validators.maxLength(20)])
-
+      'Respuesta': new FormControl("",[Validators.required,Validators.minLength(5),Validators.maxLength(30)])
     })
+   }
+
+   
+   
+
+
+   get nombreU(){
+    return this.formularioRegistro.get('nombre') as FormControl;
+   }
+
+   get contra(){
+    return this.formularioRegistro.get('contraseña') as FormControl;
+   }
+
+   get confirmar_contra(){
+    return this.formularioRegistro.get('Confirmar_contraseña') as FormControl;
+   }
+
+   get correo(){
+    return this.formularioRegistro.get('Correo') as FormControl;
+   }
+
+   get respuesta(){
+    return this.formularioRegistro.get('Respuesta') as FormControl;
    }
 
   ngOnInit() {
   }
 
 
-  async Registrar(){
-    var f = this.formularioRegistro.value;
-
-
-    if(this.formularioRegistro.invalid){
-      const alert = await this.alertController.create({
-        header:'Datos incompletos',
-        message: 'Tienes que llenar todos los datos',
-        buttons: ['Aceptar'],
-      });
-  
-      await alert.present();
-      return;
-    }
-    var usuario ={
-      nombre:f.nombre,
-      contraseña:f.contraseña
-    }
-    localStorage.setItem('usuario',JSON.stringify(usuario));
-
-  }
+ 
 }
+
