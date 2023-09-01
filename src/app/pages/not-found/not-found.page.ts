a7aae08466d1401a6e16e5b97effca4100e1c3d8
JonyRiverosF <jo.riverosf@duocuc.cl>
2023-08-31 21:50:43 -0400

    notfound

--- src/app/pages/not-found/not-found.page.ts
+++ src/app/pages/not-found/not-found.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit,ViewChildren, ViewChild } from '@angular/core';
+import { AnimationController, IonCard } from '@ionic/angular';
+import type { QueryList } from '@angular/core';
+import type { Animation } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-not-found',
@@ -6,8 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.page.scss'],
 })
 export class NotFoundPage implements OnInit {
+  @ViewChild(IonCard, { read: ElementRef })
+  card!: ElementRef<HTMLIonCardElement>;
 
-  constructor() { }
+  private animation!: Animation;
+
+  constructor(private animationCtrl: AnimationController) { }
+
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(this.card.nativeElement)
+      .duration(1500)
+      .iterations(Infinity)
+      .direction('alternate')
+      .fromTo('background', 'blue', 'var(--background)');
+  }
+
+  play() {
+    this.animation.play();
+  }
+
+  pause() {
+    this.animation.pause();
+  }
+
+  stop() {
+    this.animation.stop();
+  }
+
 
   ngOnInit() {
   }
