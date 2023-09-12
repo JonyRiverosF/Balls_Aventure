486157b54a6acafe99fc4305a82d9b1163d6fd31
JonyRiverosF <jo.riverosf@duocuc.cl>
2023-09-12 00:45:00 -0300

    pj movimiento

--- src/app/pages/tutorial/tutorial.page.ts
+++ src/app/pages/tutorial/tutorial.page.ts
@@ -9,12 +9,73 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./tutorial.page.scss'],
 })
 export class TutorialPage implements OnInit {
+
+  // Variable para rastrear la posición del personaje en píxeles
+  personajePosX = 0;
+  private isMovingLeft = false;
+  private isMovingRight = false;
+  private rotationInterval: any;
+  public rotationDegrees = 0;
+  
   
   constructor(private router:Router,public alertController :AlertController) { }
+
+  
+  moverPersonaje() {
+    if (this.isMovingLeft) {
+      this.personajePosX -= 2.5; // Ajusta la cantidad de píxeles según tu preferencia
+    }
+    if (this.isMovingRight) {
+      this.personajePosX += 2.5; // Ajusta la cantidad de píxeles según tu preferencia
+    }
+  }
+
+  startMoving(direction: string) {
+    if (direction === 'izquierda') {
+      this.isMovingLeft = true;
+    } else if (direction === 'derecha') {
+      this.isMovingRight = true;
+    }
+
+    // Iniciar rotación continua
+    this.rotationInterval = setInterval(() => {
+      this.rotationDegrees += 7.5;
+       // Ajusta la cantidad de grados para la rotación
+      this.moverPersonaje();
+    }, 20); // Ajusta el intervalo según la velocidad de rotación
+  }
+
+  stopMoving(direction: string) {
+    this.isMovingLeft = false;
+    this.isMovingRight = false;
+
+    // Detener rotación y movimiento
+    clearInterval(this.rotationInterval);
+  }
+
+
 
 
   ngOnInit() {
   }
+
+
+  
+
+
+
+  /*moverPersonaje(direccion: string) {
+    // Función para mover el personaje
+    if (direccion === 'izquierda') {
+      this.personajePosX -= 10; // Mueve 10 píxeles hacia la izquierda
+    } else if (direccion === 'derecha') {
+      this.personajePosX += 10; // Mueve 10 píxeles hacia la derecha
+    }
+  }*/
+
+  
+  
+
   public alertButtons = [
      
     {header:"¿Volver al lobby?",
@@ -54,6 +115,10 @@ export class TutorialPage implements OnInit {
 
     await alert.present();
   }
+
+
+
+
 
  
 }
