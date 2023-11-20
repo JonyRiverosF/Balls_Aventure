0cf105f982aa3c78bb130d5dcd0c3455bcb8ecd5
JonyRiverosF <jo.riverosf@duocuc.cl>
2023-11-20 06:32:23 -0300

    .

--- src/app/pages/historial/historial.page.ts
+++ src/app/pages/historial/historial.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DbservicioService } from 'src/app/services/dbservicio.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AlertController, ToastController } from '@ionic/angular';
+import { IonicSlides } from '@ionic/angular';  // Asegúrate de importar IonSlides
 
 @Component({
   selector: 'app-historial',
@@ -9,60 +10,71 @@ import { AlertController, ToastController } from '@ionic/angular';
   styleUrls: ['./historial.page.scss'],
 })
 export class HistorialPage implements OnInit {
+  @ViewChild('slides') slides: IonSlides;
 
-  arregloIntentos:any =[{
+  arregloIntentos: any[] = [{
     idI: 0,
-    estrellas: 0 ,
-    tiempo:0,
-    completado:false,
-    idNiveles:0,
-    idUsuario:0
+    estrellas: 0,
+    tiempo: 0,
+    completado: false,
+    idNiveles: 0,
+    idUsuario: 0
+  }];
 
-  }]
-
-  arreglousuario:any =[{
+  arreglousuario: any[] = [{
     idU: 0,
-    nombreU: '' ,
-    correo:'',
-    contrasena:'',
-    foto:'',
-    idPregunta:0,
-    idRol:0,
-    monedas:0,
-    descripcion:'',
-    respuesta:''
-  }]
+    nombreU: '',
+    correo: '',
+    contrasena: '',
+    foto: '',
+    idPregunta: 0,
+    idRol: 0,
+    monedas: 0,
+    descripcion: '',
+    respuesta: ''
+  }];
 
-  infoUsuario:any;
+  infoUsuario: any;
 
+  slideOpts = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    spaceBetween: 10
+  };
 
-  constructor(private activatedRouter:ActivatedRoute,private router: Router, public alertController: AlertController, private toastController: ToastController,private bd:DbservicioService) {
-    this.activatedRouter.queryParams.subscribe(param =>{
-      if (this.router.getCurrentNavigation()?.extras.state){
+  constructor(
+    private activatedRouter: ActivatedRoute,
+    private router: Router,
+    public alertController: AlertController,
+    private toastController: ToastController,
+    private bd: DbservicioService
+  ) {
+    this.activatedRouter.queryParams.subscribe(param => {
+      if (this.router.getCurrentNavigation()?.extras.state) {
         this.infoUsuario = this.router.getCurrentNavigation()?.extras?.state?.["infoUsuario"];
-        
-       
       }
-    })
-   }
-
-  ngOnInit() {
-    this.bd.bdstate().subscribe(res=>{
-      if(res){
-        this.bd.fetchIntento().subscribe(datos=>{
-          this.arregloIntentos=datos.filter(item=> item.idUsuario == this.infoUsuario.idU);
-        })
-      }
-    })
-    this.bd.bdstate().subscribe(res=>{
-      if(res){
-        this.bd.fetchUsuario().subscribe(datos=>{
-          this.arreglousuario=datos.filter(item=> item.idU == this.infoUsuario.idU);
-        })
-      }
-    })
+    });
   }
 
-  
+  ngOnInit() {
+    this.bd.bdstate().subscribe(res => {
+      if (res) {
+        this.bd.fetchIntento().subscribe(datos => {
+          this.arregloIntentos = datos.filter(item => item.idUsuario == this.infoUsuario.idU);
+        });
+      }
+    });
 
+    this.bd.bdstate().subscribe(res => {
+      if (res) {
+        this.bd.fetchUsuario().subscribe(datos => {
+          this.arreglousuario = datos.filter(item => item.idU == this.infoUsuario.idU);
+        });
+      }
+    });
+  }
+
+  irASiguienteSlide() {
+    this.slides.slideNext();
+  }
 }
